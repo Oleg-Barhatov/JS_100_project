0a40c5ea81420b88b18a543aeca50b359816136a
Oleg Barhatov <oleg-barhatov@yandex.ru>
2024-01-30 20:02:38 +0300

    project is ready

--- src/scripts/Pixel generate.js
+++ src/scripts/Pixel generate.js
@@ -1,55 +1,67 @@
 const grid = document.querySelector('.grid')
-const width = document.querySelector('.slider-input__width')
-const heigth = document.querySelector('.slider-input__heigth')
+const widthGrid = document.querySelector('.slider-input__width')
+const heigthGrid = document.querySelector('.slider-input__heigth')
 const btnCreate = document.querySelector('.btnCreate')
 const btnDelete = document.querySelector('.btnDelete')
-const btnClear = document.querySelector('.btnClear')
+const btnEraser = document.querySelector('.btnClear')
 const btnPaint = document.querySelector('.btnPaint')
 const btnColor = document.querySelector('.input-color')
 const widthValue = document.querySelector('.slider_width')
 const heigthValue = document.querySelector('.slider_heigth')
 
 let painting = false
+let eraser = false
 
 btnDelete.addEventListener('click', () => {
 	grid.textContent = ''
 })
 
-width.addEventListener('input', () => {
-	widthValue.textContent = width.value
+btnEraser.addEventListener('click', () => {
+	eraser = true
 })
 
-heigth.addEventListener('input', () => {
-	heigthValue.textContent = heigth.value
+btnPaint.addEventListener('click', () => {
+	eraser = false
+})
+
+widthGrid.addEventListener('input', () => {
+	widthValue.textContent = widthGrid.value
+})
+
+heigthGrid.addEventListener('input', () => {
+	heigthValue.textContent = heigthGrid.value
 })
 
 btnCreate.addEventListener('click', () => {
 	grid.textContent = ''
 	let count = 0
-	for (let i = 0; i < heigth.value; i++) {
-		count += 2
-		const row = document.createElement('div')
-		row.classList.add('gridRow')
+	for (let i = 0; i < heigthGrid.value; i++) {
+		count += 1
+		const rowGrid = document.createElement('div')
+		rowGrid.classList.add('gridRow')
 
-		for (let j = 0; j < width.value; j++) {
-			count += 2
+		for (let j = 0; j < widthGrid.value; j++) {
+			count += 1
 			const column = document.createElement('div')
 			column.classList.add('gridColumn')
 			column.setAttribute('id', `${count}`)
 			OnMouseMove(column)
 			OnMouseDown(column)
 			OnMouseUp(column)
-
-			row.appendChild(column)
+			rowGrid.appendChild(column)
 		}
-		grid.appendChild(row)
+		grid.appendChild(rowGrid)
 	}
 })
 
 function OnMouseDown(element) {
 	element.addEventListener('mousedown', () => {
 		painting = true
-		element.style.backgroundColor = btnColor.value
+		if (eraser) {
+			element.style.backgroundColor = 'transparent'
+		} else {
+			element.style.backgroundColor = btnColor.value
+		}
 	})
 }
 
@@ -58,8 +70,10 @@ function OnMouseMove(element) {
 		const idItem = document.elementFromPoint(e.clientX, e.clientY).id
 		const gridItem = document.querySelectorAll('.gridColumn')
 		gridItem.forEach(item => {
-			if (idItem === item.id && painting) {
+			if (idItem === item.id && painting && !eraser) {
 				item.style.backgroundColor = btnColor.value
+			} else if (idItem === item.id && painting && eraser) {
+				item.style.backgroundColor = 'transparent'
 			}
 		})
 	})
